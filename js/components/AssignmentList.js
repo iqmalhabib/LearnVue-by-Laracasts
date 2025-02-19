3f25122d0e6deb7d1625ba187da519ff9d11f25f
iqmalhabib11 <iqmalhabib11@gmail.com>
2025-02-19 17:57:13 +0800

    update

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -3,9 +3,20 @@ export default {
     components : { Assignment },
     template : `
         <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
+            <h2 class="font-bold mb-2">
+                {{ title }}
+                <span>({{ assignments.length}})</span>
+            </h2>
 
-            <ul>
+            <div class="flex gap-2">
+                <button 
+                    @click="currentTag = tag"
+                    v-for="tag in tags" 
+                    class="border rounded px-1 py-px text-xs"
+                >{{ tag }}</button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment
                     v-for="assignment in assignments"
                     :key="assignment.id"
@@ -18,5 +29,15 @@ export default {
     props : {
         assignments : Array,
         title : String
+    },
+    data() {
+        return{
+            currentTag:'' 
+        };
+    },
+    computed:{
+        tags(){
+            return new Set(this.assignments.map(a => a.tag)); //make unique tag
+        }
     }
 }
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,19 +1,24 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
 export default {
-    components : { AssignmentList }, //register as component
+    components : { AssignmentList , AssignmentCreate }, //register as component
     template : `
         <section class="space-y-6">
             <assignment-list :assignments = "filter.inProgress" title = "In Progress"></assignment-list>
             <assignment-list :assignments = "filter.completed" title = "Completed"></assignment-list>
+
+            <assignment-create @add="add"></assignment-create>
         </section>
         `,
     data(){
         return{
             assignments:[
-                { name:'Finish project', complete: false, id: 1 },
-                { name:'Read Chapter 4', complete: false, id: 2 },
-                { name:'Turn in homework', complete: false, id: 3 },
-            ]
+                { name:'Finish project', complete: false, id: 1, tag: 'math' },
+                { name:'Read Chapter 4', complete: false, id: 2, tag: 'science' },
+                { name:'Turn in homework', complete: false, id: 3, tag: 'math' },
+            ],
+           
         }
     },
     computed:{
@@ -23,5 +28,14 @@ export default {
                 completed: this.assignments.filter(assignment => assignment.complete)
             };
         }  
+    },
+    methods: {
+        add(name){
+            this.assignments.push({
+                name : name,
+                completed : false,
+                id : this.assignments.lenght + 1
+            });
+        }
     }
 }
